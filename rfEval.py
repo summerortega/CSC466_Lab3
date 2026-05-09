82c424da8ada4baca6ea223fc9b1dac46e10019d
Diego Melgoza <drmelgoza@icloud.com>
2026-05-09 10:55:25 -0700

    implement 80-20 split

--- rfEval.py
+++ rfEval.py
@@ -4,5 +4,11 @@ from randomForest import RandomForest
 from readcsv import read_csv
 
 def main(csv_file, output_file="evalReport.pdf", grid_file=None):
+    #read the data
     x, y, a = read_csv(csv_file)
-    
+    #perform the 80-20 split on the data
+    x_test = x.sample(frac=0.2)
+    y_test = y.loc[x_test.index]
+    x_train = x.drop(x_test.index)
+    y_train = y.loc[x_train.index]
+    #perform the hyperparameter tuning
